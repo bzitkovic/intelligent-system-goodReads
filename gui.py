115cb36c067d1f3f908051c5c280d4fa7e4553dd
vhrvoje2 <vhrvoje2@yahoo.com>
2021-12-28 20:56:22 +0100

    Refactored gui file to new GUI class, implemented popup window, removed recommendations file

--- gui.py
+++ gui.py
@@ -1,231 +1,368 @@
-from tkinter import Tk, Entry, Label, Button, Checkbutton, IntVar, StringVar
+from tkinter import (
+    Entry,
+    Label,
+    Button,
+    Checkbutton,
+    IntVar,
+    StringVar,
+    Frame,
+    Toplevel,
+    font,
+)
+from tkinter.constants import CENTER
 from visualization import (
     get_box_plot,
     get_heat_map,
     get_histogram_genres,
     get_tabloo_table,
 )
-from recommendation import get_all_user_options
+
+WINDOW_TITLE = "Good Reads"
+WINDOW_X_OFFSET = int(1080 / 3 - 100)
+WINDOW_Y_OFFSET = int(1920 / 2 - 400)
+WINDOW_IS_RESIZABLE = False
+FONT_STYLE = "Ariel"
+FONT_SIZE = 11
+COLOR_PURPLE = "#7878ab"
+COLOR_GREEN = "#78ab7c"
 
 
-def set_gui(books, dataframe):
-    window = Tk()
-    window.title("Good Reads")
+class GUI:
+    def __init__(self, window, books, dataframe):
+        self.window = window
+        self.books = books
+        self.dataframe = dataframe
+        self.configure_window()
+        self.create_variables()
+        self.create_UI()
 
-    chk1_variable = IntVar()
-    chk2_variable = IntVar()
-    chk3_variable = IntVar()
-    chk4_variable = IntVar()
-    chk5_variable = IntVar()
-    chk6_variable = IntVar()
+    def configure_window(self):
+        self.window.title(WINDOW_TITLE)
+        self.window.geometry(f"800x400+{WINDOW_Y_OFFSET}+{WINDOW_X_OFFSET}")
+        self.window.configure(bg=COLOR_PURPLE)
+        self.window.resizable(WINDOW_IS_RESIZABLE, WINDOW_IS_RESIZABLE)
 
-    ent_number_of_pages_variable = StringVar()
-    ent_minimum_rating_variable = StringVar()
-    ent_minimum_reviews_variable = StringVar()
-    ent_minimum_total_ratings_variable = StringVar()
+        default_font = font.nametofont("TkDefaultFont")
+        default_font.configure(family=FONT_STYLE, size=FONT_SIZE)
 
-    chk_options = [
-        chk1_variable,
-        chk2_variable,
-        chk3_variable,
-        chk4_variable,
-        chk5_variable,
-        chk6_variable,
-    ]
+    def create_variables(self):
+        self.chk1_variable = IntVar()
+        self.chk2_variable = IntVar()
+        self.chk3_variable = IntVar()
+        self.chk4_variable = IntVar()
+        self.chk5_variable = IntVar()
+        self.chk6_variable = IntVar()
 
-    ent_values = [ent_number_of_pages_variable]
+        self.ent_number_of_pages_variable = StringVar()
+        self.ent_minimum_rating_variable = StringVar()
+        self.ent_minimum_reviews_variable = StringVar()
+        self.ent_minimum_total_ratings_variable = StringVar()
 
-    # User entry
-    lbl_number = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Number of pages:"
-    )
+        self.chk_options = [
+            self.chk1_variable,
+            self.chk2_variable,
+            self.chk3_variable,
+            self.chk4_variable,
+            self.chk5_variable,
+            self.chk6_variable,
+        ]
 
-    lbl_number.grid(row=0, column=2, padx=20, sticky="W")
+        self.ent_values = [
+            self.ent_number_of_pages_variable,
+            self.ent_minimum_rating_variable,
+            self.ent_minimum_reviews_variable,
+            self.ent_minimum_total_ratings_variable,
+        ]
 
-    ent_number_of_pages = Entry(
-        window, width=20, textvariable=ent_number_of_pages_variable
-    )
+    def create_UI(self):
+        self.user_entry_frame = Frame(self.window, bg=COLOR_PURPLE)
+        self.user_entry_frame.grid(row=0, column=1, pady=10, sticky="NEWS")
 
-    ent_number_of_pages.grid(row=1, column=2, padx=25, pady=10, sticky="W")
+        self.visualization_frame = Frame(self.window, bg=COLOR_PURPLE)
+        self.visualization_frame.grid(
+            row=0, rowspan=2, column=0, pady=10, sticky="NEWS"
+        )
 
-    lbl_rating = Label(window, font=("Ariel", 12), bg="#7878ab", text="Minimun rating:")
+        self.checkbox_frame = Frame(self.window, bg=COLOR_PURPLE)
+        self.checkbox_frame.grid(row=1, column=1, sticky="NEWS")
 
-    lbl_rating.grid(row=2, column=2, padx=20, sticky="W")
+        self.recommend_frame = Frame(self.window, bg=COLOR_PURPLE)
+        self.recommend_frame.grid(row=2, column=0, columnspan=2, pady=35, sticky="NEWS")
 
-    ent_rating = Entry(window, width=20, textvariable=ent_minimum_rating_variable)
+        self.create_visualization()
+        self.create_user_entry()
+        self.create_checkboxes()
+        self.create_recommend_button()
 
-    ent_rating.grid(row=3, column=2, padx=25, pady=10, sticky="W")
+    def create_user_entry(self):
+        self.lbl_number = Label(
+            self.user_entry_frame, bg=COLOR_PURPLE, text="Number of pages:"
+        )
+        self.lbl_number.grid(row=0, column=0, padx=20, sticky="W")
 
-    lbl_reviews = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Minimun number od reviews:"
-    )
+        self.ent_number_of_pages = Entry(
+            self.user_entry_frame,
+            width=30,
+            textvariable=self.ent_number_of_pages_variable,
+        )
+        self.ent_number_of_pages.grid(row=1, column=0, padx=25, pady=10, sticky="W")
 
-    lbl_reviews.grid(row=0, column=3, padx=20, sticky="W")
+        self.lbl_rating = Label(
+            self.user_entry_frame, bg=COLOR_PURPLE, text="Minimum rating:"
+        )
+        self.lbl_rating.grid(row=2, column=0, padx=20, sticky="W")
 
-    ent_reviews = Entry(window, width=20, textvariable=ent_minimum_reviews_variable)
+        self.ent_rating = Entry(
+            self.user_entry_frame,
+            width=30,
+            textvariable=self.ent_minimum_rating_variable,
+        )
+        self.ent_rating.grid(row=3, column=0, padx=25, pady=10, sticky="W")
 
-    ent_reviews.grid(row=1, column=3, padx=25, pady=10, sticky="W")
+        self.lbl_reviews = Label(
+            self.user_entry_frame,
+            bg=COLOR_PURPLE,
+            text="Minimum number od reviews:",
+        )
+        self.lbl_reviews.grid(row=0, column=1, padx=20, sticky="W")
 
-    lbl_total_ratings = Label(
-        window,
-        font=("Ariel", 12),
-        bg="#7878ab",
-        text="Minimun number od total ratings:",
-    )
+        self.ent_reviews = Entry(
+            self.user_entry_frame,
+            width=30,
+            textvariable=self.ent_minimum_reviews_variable,
+        )
+        self.ent_reviews.grid(row=1, column=1, padx=25, pady=10, sticky="W")
 
-    lbl_total_ratings.grid(row=2, column=3, padx=20, sticky="W")
+        self.lbl_total_ratings = Label(
+            self.user_entry_frame,
+            bg=COLOR_PURPLE,
+            text="Minimum number od total ratings:",
+        )
+        self.lbl_total_ratings.grid(row=2, column=1, padx=20, sticky="W")
 
-    ent_total_ratings = Entry(
-        window, width=20, textvariable=ent_minimum_total_ratings_variable
-    )
+        self.ent_total_ratings = Entry(
+            self.user_entry_frame,
+            width=30,
+            textvariable=self.ent_minimum_total_ratings_variable,
+        )
+        self.ent_total_ratings.grid(row=3, column=1, padx=25, pady=10, sticky="W")
 
-    ent_total_ratings.grid(row=3, column=3, padx=25, pady=10, sticky="W")
+    def create_checkboxes(self):
+        self.lbl_genre = Label(
+            self.checkbox_frame, bg=COLOR_PURPLE, text="Select prefered genre:"
+        )
+        self.lbl_genre.grid(row=0, column=0, padx=20, sticky="W")
 
-    lbl_genre = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Select prefered genre:"
-    )
+        self.chk1 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="Romance",
+            variable=self.chk1_variable,
+        )
+        self.chk1.grid(row=1, column=0, padx=25, pady=5, sticky="W")
 
-    lbl_genre.grid(row=4, column=2, padx=20, sticky="W")
+        self.chk2 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="Fiction",
+            variable=self.chk2_variable,
+        )
+        self.chk2.grid(row=1, column=1, pady=5, sticky="W")
 
-    chk1 = Checkbutton(window, text="Romance", variable=chk1_variable)
+        self.chk3 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="Fantasy",
+            variable=self.chk3_variable,
+        )
+        self.chk3.grid(row=2, column=0, padx=25, pady=5, sticky="W")
 
-    chk1.grid(row=5, column=2, padx=25, pady=10, sticky="W")
+        self.chk4 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="Nonfiction",
+            variable=self.chk4_variable,
+        )
+        self.chk4.grid(row=2, column=1, pady=5, sticky="W")
 
-    chk2 = Checkbutton(window, text="History", variable=chk2_variable)
+        self.chk5 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="History",
+            variable=self.chk5_variable,
+        )
+        self.chk5.grid(row=3, column=0, padx=25, pady=5, sticky="W")
 
-    chk2.grid(row=5, column=2, pady=10, sticky="E")
+        self.chk6 = Checkbutton(
+            self.checkbox_frame,
+            bg=COLOR_PURPLE,
+            text="Childrens",
+            variable=self.chk6_variable,
+        )
+        self.chk6.grid(row=3, column=1, pady=5, sticky="W")
 
-    chk3 = Checkbutton(window, text="Nonfiction", variable=chk3_variable)
+    def create_visualization(self):
+        self.lbl_get_heat_map = Label(
+            self.visualization_frame, bg=COLOR_PURPLE, text="Get heat map"
+        )
+        self.lbl_get_heat_map.grid(row=0, column=0, padx=20, pady=10, sticky="W")
 
-    chk3.grid(row=6, column=2, padx=25, pady=10, sticky="W")
+        self.btn_get_heat_map = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_heat_map(self.dataframe),
+        )
+        self.btn_get_heat_map.grid(row=0, column=1)
 
-    chk4 = Checkbutton(window, text="Fantasy", variable=chk4_variable)
+        self.lbl_get_boxplot_rating = Label(
+            self.visualization_frame, bg=COLOR_PURPLE, text="Get boxplot for rating"
+        )
+        self.lbl_get_boxplot_rating.grid(row=1, column=0, padx=20, pady=10, sticky="W")
 
-    chk4.grid(row=6, column=2, pady=10, sticky="E")
+        self.btn_get_boxplot_rating = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_box_plot(self.dataframe, "rating"),
+        )
+        self.btn_get_boxplot_rating.grid(row=1, column=1)
 
-    chk5 = Checkbutton(window, text="Fiction", variable=chk5_variable)
+        self.lbl_get_boxplot_reviews = Label(
+            self.visualization_frame,
+            bg=COLOR_PURPLE,
+            text="Get boxplot for reviews",
+        )
+        self.lbl_get_boxplot_reviews.grid(row=2, column=0, padx=20, pady=10, sticky="W")
 
-    chk5.grid(row=7, column=2, padx=25, pady=10, sticky="W")
+        self.btn_get_boxplot_reviews = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_box_plot(self.dataframe, "reviews"),
+        )
+        self.btn_get_boxplot_reviews.grid(row=2, column=1)
 
-    chk6 = Checkbutton(window, text="Childrens", variable=chk6_variable)
+        self.lbl_get_boxplot_total_ratings = Label(
+            self.visualization_frame,
+            bg=COLOR_PURPLE,
+            text="Get boxplot for total ratings",
+        )
+        self.lbl_get_boxplot_total_ratings.grid(
+            row=3, column=0, padx=20, pady=10, sticky="W"
+        )
 
-    chk6.grid(row=7, column=2, pady=10, sticky="E")
+        self.btn_get_boxplot_total_ratings = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_box_plot(self.dataframe, "totalratings"),
+        )
+        self.btn_get_boxplot_total_ratings.grid(row=3, column=1)
 
-    btn_get_user_entry = Button(
-        window,
-        text="Recommend me a book",
-        width=25,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_all_user_options(chk_options, ent_values, dataframe),
-    )
+        self.lbl_get_top_genres = Label(
+            self.visualization_frame, bg=COLOR_PURPLE, text="Get top 8 genres"
+        )
+        self.lbl_get_top_genres.grid(row=4, column=0, padx=20, pady=10, sticky="W")
 
-    btn_get_user_entry.grid(row=7, column=3, padx=25, pady=10, sticky="W")
+        self.btn_get_top_genres = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_histogram_genres(self.books),
+        )
+        self.btn_get_top_genres.grid(row=4, column=1)
 
-    # Visualization
-    lbl_get_heat_map = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Get heat map"
-    )
+        self.lbl_get_tabloo_table = Label(
+            self.visualization_frame, bg=COLOR_PURPLE, text="Get table for data"
+        )
+        self.lbl_get_tabloo_table.grid(row=5, column=0, padx=20, pady=10, sticky="W")
 
-    lbl_get_heat_map.grid(row=0, column=0, padx=20, pady=10, sticky="W")
+        self.btn_get_tabloo_table = Button(
+            self.visualization_frame,
+            text="➔",
+            width=6,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_tabloo_table(self.dataframe),
+        )
+        self.btn_get_tabloo_table.grid(row=5, column=1, padx=20, pady=10)
 
-    btn_get_heat_map = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_heat_map(dataframe),
-    )
+    def create_recommend_button(self):
+        self.btn_get_user_entry = Button(
+            self.recommend_frame,
+            text="Recommend me a book",
+            width=25,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: self.recommend_books(),
+        )
+        self.btn_get_user_entry.pack()
 
-    btn_get_heat_map.grid(row=0, column=1)
+    def recommend_books(self):
+        checked_options = self.get_checked_options()
+        # TODO: get entry values
 
-    lbl_get_boxplot_rating = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Get boxplot for rating"
-    )
+        # TODO: create dataframe with recommended books
+        # TODO: pass dataframe and message to create_popup
+        self.create_popup(
+            self.dataframe,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        )
 
-    lbl_get_boxplot_rating.grid(row=1, column=0, padx=20, pady=10, sticky="W")
+    def create_popup(self, dataframe, message):
+        self.popup_window = Toplevel(self.window, bg=COLOR_PURPLE)
+        self.popup_window.title("Recommended books")
+        self.popup_window.geometry(
+            f"600x200+{WINDOW_Y_OFFSET+100}+{WINDOW_X_OFFSET+75}"
+        )
 
-    btn_get_boxplot_rating = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_box_plot(dataframe, "rating"),
-    )
+        self.popup_frame = Frame(self.popup_window, bg=COLOR_PURPLE)
+        self.popup_frame.grid(row=0, column=0, pady=30, padx=55)
 
-    btn_get_boxplot_rating.grid(row=1, column=1)
+        self.recommended_message = Label(
+            self.popup_frame,
+            text=message,
+            justify=CENTER,
+            bg=COLOR_PURPLE,
+            wraplength=500,
+        )
+        self.recommended_message.pack()
 
-    lbl_get_boxplot_reviews = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Get boxplot for reviews"
-    )
+        self.popup_button = Button(
+            self.popup_window,
+            text="Show recommendations",
+            width=25,
+            bd=3,
+            bg=COLOR_GREEN,
+            command=lambda: get_tabloo_table(dataframe),
+        )
+        self.popup_button.grid(row=1, column=0)
 
-    lbl_get_boxplot_reviews.grid(row=2, column=0, padx=20, pady=10, sticky="W")
+    def get_checked_options(self):
+        chk_checked_values = []
 
-    btn_get_boxplot_reviews = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_box_plot(dataframe, "reviews"),
-    )
+        if self.chk_options[0].get() == 1:
+            chk_checked_values.append("Romance")
+        if self.chk_options[1].get() == 1:
+            chk_checked_values.append("Fiction")
+        if self.chk_options[2].get() == 1:
+            chk_checked_values.append("Fantasy")
+        if self.chk_options[3].get() == 1:
+            chk_checked_values.append("Nonfiction")
+        if self.chk_options[4].get() == 1:
+            chk_checked_values.append("History")
+        if self.chk_options[5].get() == 1:
+            chk_checked_values.append("Childrens")
 
-    btn_get_boxplot_reviews.grid(row=2, column=1, padx=20, pady=10)
-
-    lbl_get_boxplot_total_ratings = Label(
-        window,
-        font=("Ariel", 12),
-        bg="#7878ab",
-        text="Get boxplot for total ratings",
-    )
-
-    lbl_get_boxplot_total_ratings.grid(row=3, column=0, padx=20, pady=10, sticky="W")
-
-    btn_get_boxplot_total_ratings = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_box_plot(dataframe, "totalratings"),
-    )
-
-    btn_get_boxplot_total_ratings.grid(row=3, column=1, padx=20, pady=10)
-
-    lbl_get_top_genres = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Get top 8 genres"
-    )
-
-    lbl_get_top_genres.grid(row=4, column=0, padx=20, pady=10, sticky="W")
-
-    btn_get_top_genres = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_histogram_genres(books),
-    )
-
-    btn_get_top_genres.grid(row=4, column=1, padx=20, pady=10)
-
-    lbl_get_tabloo_table = Label(
-        window, font=("Ariel", 12), bg="#7878ab", text="Get table for data"
-    )
-
-    lbl_get_tabloo_table.grid(row=5, column=0, padx=20, pady=10, sticky="W")
-
-    btn_get_tabloo_table = Button(
-        window,
-        text="➔",
-        width=6,
-        bd=3,
-        bg="#78ab7c",
-        command=lambda: get_tabloo_table(dataframe),
-    )
-
-    btn_get_tabloo_table.grid(row=5, column=1, padx=20, pady=10)
-
-    return window
+        return chk_checked_values
